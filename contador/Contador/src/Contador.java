d6de38b42421cdef04b2335cda45bd75cc1103dc
Marlon Ricardo <67763666+marlonricardo@users.noreply.github.com>
2023-11-16 02:07:34 -0300

    DIO Desafio

---
+++ contador/Contador/src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro: ");
+        int parametroUm = scan.nextInt();
+        System.out.println("Digite o segundo parametro: ");
+        int parametroDois = scan.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for(int i = 0; i < contagem; i++){
+            System.out.println("Imprimindo o número " + (i + 1));
+        }
+    }
+}
